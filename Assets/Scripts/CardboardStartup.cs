7d75c92b0cc5ba46c44da5b676c670b522c2ea16
coolergate <coolergate@gmail.com>
2023-05-13 21:59:33 -0300

    Traduzir comentários dos scripts

--- Assets/Scripts/CardboardStartup.cs
+++ Assets/Scripts/CardboardStartup.cs
@@ -1,55 +1,31 @@
-//-----------------------------------------------------------------------
-// <copyright file="CardboardStartup.cs" company="Google LLC">
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
-//-----------------------------------------------------------------------
-
 using Google.XR.Cardboard;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// Initializes Cardboard XR Plugin.
+/// Inicia o plugin Cardboard XR.
 /// </summary>
 public class CardboardStartup : MonoBehaviour
 {
-	/// <summary>
-	/// Start is called before the first frame update.
-	/// </summary>
 	public void Start()
 	{
-		// Configures the app to not shut down the screen and sets the brightness to maximum.
-		// Brightness control is expected to work only in iOS, see:
+		// Configura o aplicativo para não desligar a tela e aumenta o brilho no máximo.
+		// Controle de brilho é esperado para somente funcionar no iOS, olhe:
 		// https://docs.unity3d.com/ScriptReference/Screen-brightness.html.
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		Screen.brightness = 1.0f;
 
-		// Checks if the device parameters are stored and scans them if not.
+		// Verifica se o dispositivo tem os parâmetros guardados e escaneia caso não.
 		if (!Api.HasDeviceParams())
 		{
 			Api.ScanDeviceParams();
 		}
 
-		// Add the player from another scene
+		// Adicionar o jogador de outra cena.
 		SceneManager.LoadScene(1, LoadSceneMode.Additive);
 	}
 
-	/// <summary>
-	/// Update is called once per frame.
-	/// </summary>
 	public void Update()
 	{
 
--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,21 +1,3 @@
-//-----------------------------------------------------------------------
-// <copyright file="VrModeController.cs" company="Google LLC">
-// Copyright 2020 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
-//-----------------------------------------------------------------------
-
 using System.Collections;
 using Google.XR.Cardboard;
 using UnityEngine;
@@ -25,36 +7,33 @@ using UnityEngine.XR.Management;
 using UnityEngine.UI;
 
 /// <summary>
-/// Turns VR mode on and off.
+/// Módulo que controla a perspectiva do jogador em primeira pessoa
 /// </summary>
 public class FirstPersonController : MonoBehaviour
 {
-	// Field of view value to be used when the scene is not in VR mode. In case
-	// XR isn't initialized on startup, this value could be taken from the main
-	// camera and stored.
+	// Campo de visão para quando o modo VR não estiver ativo
 	private const float _defaultFieldOfView = 60.0f;
 
-	// Main camera from the scene.
 	private Camera _Camera;
 
-	// Mouse movement for PC Users
+	// Movimento do mouse para computador
 	public float MouseSensitivity = 2.0f;
 	private float MouseX = 0.0f, MouseY = 0.0f;
 
-	// Player movement properties
+	// Propriedades de movimento do jogador
 	CharacterController _Controller;
 	public float MovementSpeed = 10.0f;
 
-	// Raycasting to replace the default cardboard script
+	// Ponteiro para indicar objeto que está sendo apontado
 	private GameObject _CurrentGazedObject;
 
-    // Changing crosshair colors when gazing upon an object
+    // Propriedades do crosshair para alterar quando tiver um objeto ativo
     public Image CrosshairImage;
 	public Color CrosshairDefaultColor = new Color(1.0f, 1.0f, 1.0f);
 	public Color CrosshairGazedColor = new Color(1.0f, 0.0f, 0.0f);
 
 	/// <summary>
-	/// Gets a value indicating whether the screen has been touched this frame.
+	/// Retorna se a tela está sendo tocada neste frame
 	/// </summary>
 	private bool _isScreenTouched
 	{
@@ -65,7 +44,7 @@ public class FirstPersonController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Gets a value indicating whether the VR mode is enabled.
+	/// Retorna uma variável indicando se o modo VR está ativo
 	/// </summary>
 	private bool _isVrModeEnabled
 	{
@@ -75,26 +54,24 @@ public class FirstPersonController : MonoBehaviour
 		}
 	}
 
-	/// <summary>
-	/// Start is called before the first frame update.
-	/// </summary>
 	public void Start()
 	{
-		// Saves the main camera from the scene.
+		// Salva a câmera principal da cena
 		_Camera = Camera.main;
 
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		Screen.brightness = 1.0f;
 
-		// Checks if the device parameters are stored and scans them if not.
-		// This is only required if the XR plugin is initialized on startup,
-		// otherwise these API calls can be removed and just be used when the XR
-		// plugin is started.
+		// Verifica se os parãmetros do dispositivo está guardado, scanea se não.
+		// Isso é somente obrigatório se o plugin XR está ativo na inicialização,
+		// se não esses chamados da API podem ser removidos e somente ser usado
+		// quando o plugin XR estiver ativo.
 		if (!Api.HasDeviceParams())
 		{
 			Api.ScanDeviceParams();
 		}
 
+		// Remover o cursor da tela e trancar para que não saia do centro
 		Cursor.visible = true;
 		Cursor.lockState = CursorLockMode.Locked;
 
@@ -103,7 +80,7 @@ public class FirstPersonController : MonoBehaviour
 
 	public void FixedUpdate()
 	{
-		// Movement
+		// Movimentação
 		if (Camera.main != null)
 		{
 			float Horizontal = Input.GetAxis("Horizontal");
@@ -112,7 +89,8 @@ public class FirstPersonController : MonoBehaviour
 			Transform Orientation = Camera.main.transform;
 			Vector3 Direction = Orientation.forward * Vertical + Orientation.right * Horizontal;
 
-			Direction.y = 0; // Basically remove noclip movement
+			// Remover movimento vertical para que o jogador não fique voando pela cena
+			Direction.y = 0;
 
 			_Controller.Move(Direction * MovementSpeed * Time.deltaTime);
 			transform.parent.transform.eulerAngles = new Vector3();
@@ -129,40 +107,48 @@ public class FirstPersonController : MonoBehaviour
 			transform.eulerAngles = new Vector3(MouseY, MouseX, 0);
 		}
 
-		// raycasting to interact with the world objects
+		// Raycast para interagir com os objetos da cena
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
 		{
-			// GameObject detected in front of the camera.
+
 			if (hit.transform.gameObject != _CurrentGazedObject)
 			{
-				// New GameObject.
+
 				if (_CurrentGazedObject != null) SendMessage(_CurrentGazedObject, "OnPointerExit");
 
 				_CurrentGazedObject = hit.transform.gameObject;
 				SendMessage(_CurrentGazedObject, "OnPointerEnter");
+
 			}
+
 		}
 		else
 		{
-			// No GameObject detected in front of the camera.
+
+			// Nenhum objeto detectado em frente a câmera
 			if (_CurrentGazedObject != null) SendMessage(_CurrentGazedObject, "OnPointerExit");
 			_CurrentGazedObject = null;
+
 		}
 
 		if (_CurrentGazedObject != null)
 		{
+
 			if (CrosshairImage != null) CrosshairImage.color = CrosshairGazedColor;
 
 			bool InputIsDown = Input.GetMouseButtonDown(0) || Input.GetButtonDown("Attack1") || Input.touchCount > 0;
 			if (InputIsDown) SendMessage(_CurrentGazedObject, "OnPointerClick");
+			
 		}
 		else
 		{
+
 			if (CrosshairImage != null) CrosshairImage.color = CrosshairDefaultColor;
+
 		}
 
-		// Foward this line is VR only functions for only android and OpenGLES3
+		// Desta linha para frente é somente para VR / XR no Android
 		if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES3 || Application.platform != RuntimePlatform.Android) return;
 
 		if (_isVrModeEnabled)
@@ -190,7 +176,7 @@ public class FirstPersonController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Enters VR mode.
+	/// Entrar no modo VR.
 	/// </summary>
 	private void EnterVR()
 	{
@@ -202,7 +188,7 @@ public class FirstPersonController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Exits VR mode.
+	/// Sair do modo VR.
 	/// </summary>
 	private void ExitVR()
 	{
@@ -210,12 +196,12 @@ public class FirstPersonController : MonoBehaviour
 	}
 
 	/// <summary>
-	/// Initializes and starts the Cardboard XR plugin.
-	/// See https://docs.unity3d.com/Packages/com.unity.xr.management@3.2/manual/index.html.
+	/// Inicializa o plugin Cardboard XR
+	/// Olhe https://docs.unity3d.com/Packages/com.unity.xr.management@3.2/manual/index.html.
 	/// </summary>
 	///
 	/// <returns>
-	/// Returns result value of <c>InitializeLoader</c> method from the XR General Settings Manager.
+	/// Retorna a variável do método <c>InitializeLoader</c> do arquivo de configação XR Geral.
 	/// </returns>
 	private IEnumerator StartXR()
 	{
@@ -238,7 +224,8 @@ public class FirstPersonController : MonoBehaviour
 
 	/// <summary>
 	/// Stops and deinitializes the Cardboard XR plugin.
-	/// See https://docs.unity3d.com/Packages/com.unity.xr.management@3.2/manual/index.html.
+	/// Encerra o plugin Cardboard XR
+	/// Olhe https://docs.unity3d.com/Packages/com.unity.xr.management@3.2/manual/index.html.
 	/// </summary>
 	private void StopXR()
 	{
