cb6401e4676700f5b64492665a6d4e547b33c292
coolergate <coolergate@gmail.com>
2023-05-17 12:18:21 -0300

    Melhorar suporte mobile

--- Assets/Scripts/CardboardStartup.cs
+++ Assets/Scripts/CardboardStartup.cs
@@ -10,17 +10,8 @@ public class CardboardStartup : MonoBehaviour
 {
 	public void Start()
 	{
-		// Configura o aplicativo para nao desligar a tela e aumenta o brilho no maximo.
-		// Controle de brilho e esperado para somente funcionar no iOS, olhe:
-		// https://docs.unity3d.com/ScriptReference/Screen-brightness.html.
-		Screen.sleepTimeout = SleepTimeout.NeverSleep;
-		Screen.brightness = 1.0f;
-
-		// Verifica se o dispositivo tem os par�metros guardados e escaneia caso n�o.
-		if (!Api.HasDeviceParams())
-		{
-			Api.ScanDeviceParams();
-		}
+		Screen.sleepTimeout = SleepTimeout.NeverSleep; // fazer com que a tela nao desligue
+		Screen.brightness = 1.0f; // so funciona no iOS
 
 		// Adicionar o jogador de outra cena.
 		SceneManager.LoadScene(1, LoadSceneMode.Additive);
@@ -30,27 +21,22 @@ public class CardboardStartup : MonoBehaviour
 	{
 
 		//if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.OpenGLES3) return;
-		if (Application.platform != RuntimePlatform.Android) return;
-		
-		/*if (Api.IsGearButtonPressed)
-		{
-			Api.ScanDeviceParams();
-		}*/
+		if (!Application.isMobilePlatform) return;
 
 		if (Api.IsCloseButtonPressed)
 		{
 			Application.Quit();
 		}
 
-		if (Api.IsTriggerHeldPressed)
+		/*if (Api.IsTriggerHeldPressed)
 		{
 			Api.Recenter();
-		}
+		}*/
 
-		if (Api.HasNewDeviceParams())
+		/*if (Api.HasNewDeviceParams())
 		{
 			Api.ReloadDeviceParams();
-		}
+		}*/
 
 		Api.UpdateScreenParams();
 	}
