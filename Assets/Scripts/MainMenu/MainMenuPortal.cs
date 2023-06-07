c8063322f2d7624a5beb1664475634227a497943
coolergate <coolergate@gmail.com>
2023-06-06 21:10:20 -0300

    Progresso 06/06

--- Assets/Scripts/MainMenu/MainMenuPortal.cs
+++ Assets/Scripts/MainMenu/MainMenuPortal.cs
@@ -5,18 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuPortal : MonoBehaviour
 {
-	[Header ("Scenes")]
-	public int SceneIndex;
-	public LoadSceneMode LoadMode;
-
 	[Header ("Portal properties")]
 	public string PortalName = "";
 	public string PortalDescription = "";
-	public GameObject PreviewObject;
+	public int SceneIndex;
+
+	[Header ("Preview object properties")]
+	public MeshRenderer PreviewRenderer;
+
+	private Color _original_color;
+
+	public Color original_color
+	{
+		get {
+			return _original_color;
+		}
+	}
 
 	void Start()
 	{
-
+		_original_color = PreviewRenderer.material.color;
 	}
 
 	void Update()
--- Assets/Scripts/MainMenu/MainMenuRootObject.cs
+++ Assets/Scripts/MainMenu/MainMenuRootObject.cs
@@ -7,48 +7,77 @@ public class MainMenuRootObject : MonoBehaviour
 {
 	[Header ("Properties")]
 	public float RotationAmount = 0.0f;
+	public float RotationTime = 0.25f;
 	
 	[Header ("Interface properties")]
 	public TextMeshProUGUI DescriptionText; 
 
 	private int _index = 0;
-	private GameObject[] _children_list = {};
+	private int _portals_amount = 0;
+	private ArrayList _children_list;
 
+	private MainMenuPortal _current_portal_component;
 	private bool _allowedupdate = true;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		_children_list = new ArrayList(transform.childCount);
+
 		for (int index = 0; index < transform.childCount; index++)
 		{
 			GameObject child = transform.GetChild(index).gameObject;
 			
-			if (!child.GetComponent<MainMenuPortal>()) continue;
+			MainMenuPortal component;
+			child.TryGetComponent<MainMenuPortal>(out component);
+			if (!component) continue;
 
-			_children_list.SetValue(child, _children_list.Length + 1);
+			Color ComponentColor = component.PreviewRenderer.material.color;
+			ComponentColor.a = 0.0f;
+
+			//component.PreviewRenderer.material.color = ComponentColor;
+			component.PreviewRenderer.material.color = new Color(0, 0, 0, 0);
+
+			_portals_amount++;
+			_children_list.Add(child);
 		}
+
+		Debug.Log(_children_list.Count);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		int move_direction = 0;
+		if (!_allowedupdate) return;
 
-		if (_allowedupdate)
-		{
-			float direction = Input.GetAxis("Horizontal");
-			move_direction = direction > 0 ? 1 : -1;
-		}
-
-		if (move_direction != 0) RotateObject(move_direction);
+		float direction = Input.GetAxis("Horizontal");
+		if (direction != 0.0f) StartCoroutine(ChangeOption(direction > 0 ? -1 : 1));
 	}
 
-	private IEnumerator RotateObject(int direction)
+	private IEnumerator ChangeOption(int direction)
 	{
 		_allowedupdate = false;
-		LeanTween.rotateAround(gameObject, Vector3.up, RotationAmount * direction, 0.5f);
 
-		yield return new WaitForSeconds(0.5f);
+		LeanTween.rotateAround(gameObject, Vector3.up, RotationAmount * direction, RotationTime).setEase(LeanTweenType.easeOutQuad);
+
+		if (_current_portal_component)
+		{
+			Color component_color = _current_portal_component.PreviewRenderer.material.color;
+			component_color.a = 0.0f;
+			LeanTween.color(_current_portal_component.gameObject, new Color(1, 1, 1, 0), RotationTime * 0.5f);
+		}
+
+		yield return new WaitForSeconds(RotationTime * 0.5f);
+
+		_index += direction;
+		if (_index > _children_list.Count - 1 || _index < 0) _index = 0;
+
+		_current_portal_component = _children_list[_index] as MainMenuPortal;
+
+		if (_current_portal_component)
+			LeanTween.color(_current_portal_component.gameObject, new Color(1, 1, 1, 1), RotationTime * 0.5f);
+
+		yield return new WaitForSeconds(RotationTime * 0.5f	);
 
 		_allowedupdate = true;
 	}
