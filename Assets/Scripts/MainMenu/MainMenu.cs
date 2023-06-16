e6f478594ced184f4e16a5ec9ab64f564c9c7140
coolergate <coolergate@gmail.com>
2023-06-16 16:32:17 -0300

    Adicionar fontes

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -3,86 +3,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Management;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private const float _defaultFieldOfView = 60.0f;
-    private Camera _Camera;
+	private const float _defaultFieldOfView = 60.0f;
+	private Camera _Camera;
 
-    public void Start()
-    {
-        _Camera = Camera.main;
+	[Header("Properties")]
+	public string FirstSceneName = "";
 
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        Screen.brightness = 1.0f;
-    }
+	[Space(20)]
 
-    public void Update()
-    {
+	[Header("Interface")]
+	public Image TransitionImage;
+	public Button PlayButton;
+	public Button SettingsButton;
 
-        if (!Application.isMobilePlatform) return;
+	private bool RunningFirstTime = true;
 
-        if (UserSettings.VR_Enabled)
-        {
-            if (Api.IsCloseButtonPressed) StopXR();
+	public void Start()
+	{
+		_Camera = Camera.main;
 
-            Api.UpdateScreenParams();
-        }
-        else
-        {
-            if (UserSettings.isScreenBeingTouched) EnterVR();
-        }
-    }
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		Screen.brightness = 1.0f;
 
-    void ToggleVR()
-    {
-        if (UserSettings.VR_Enabled) StopXR();
-        else EnterVR();
-    }
+		PlayButton.onClick.AddListener(EnterScene);
+	}
 
-    private void EnterVR()
-    {
-        Debug.Log("Entering VR mode...");
+	public void Update()
+	{
 
-        StartCoroutine(StartXR());
-        if (Api.HasNewDeviceParams())
-        {
-            Api.ReloadDeviceParams();
-        }
-    }
+		if (!Application.isMobilePlatform) return;
 
-    private IEnumerator StartXR()
-    {
-        Debug.Log("Initializing XR...");
-        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+		if (UserSettings.VR_Enabled)
+		{
+			if (Api.IsCloseButtonPressed) StopXR();
 
-        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
-        {
-            Debug.LogError("Initializing XR Failed.");
-        }
-        else
-        {
-            Debug.Log("XR initialized.");
+			Api.UpdateScreenParams();
+		}
+		else
+		{
+			if (UserSettings.isScreenBeingTouched) EnterVR();
+		}
 
-            Debug.Log("Starting XR...");
-            XRGeneralSettings.Instance.Manager.StartSubsystems();
-            Debug.Log("XR started.");
-        }
-    }
+		if (RunningFirstTime && UserSettings.VR_Enabled)
+		{
+			RunningFirstTime = false;
+			StopXR();
+		}
+	}
 
-    private void StopXR()
-    {
-        Debug.Log("Exiting VR mode");
+	private void EnterScene()
+	{
+		StartCoroutine(StartStory());
+	}
 
-        Debug.Log("Stopping XR...");
-        XRGeneralSettings.Instance.Manager.StopSubsystems();
-        Debug.Log("XR stopped.");
+	private IEnumerator StartStory()
+	{
+        var current_color = TransitionImage.color;
+        current_color.a = 0;
 
-        Debug.Log("Deinitializing XR...");
-        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
-        Debug.Log("XR deinitialized.");
+        var target_color = TransitionImage.color;
+        target_color.a = 1;
 
-        _Camera.ResetAspect();
-        _Camera.fieldOfView = _defaultFieldOfView;
-    }
+		void UpdateTransitionColor(Color color)
+		{
+			TransitionImage.color = color;	
+		}
+
+		LeanTween.value(TransitionImage.gameObject, UpdateTransitionColor, current_color, target_color, 0.5f);
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(FirstSceneName);
+	}
+
+	private void EnterVR()
+	{
+		Debug.Log("Entering VR mode...");
+
+		StartCoroutine(StartXR());
+		if (Api.HasNewDeviceParams())
+		{
+			Api.ReloadDeviceParams();
+		}
+	}
+
+	private IEnumerator StartXR()
+	{
+		Debug.Log("Initializing XR...");
+		yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+		if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+		{
+			Debug.LogError("Initializing XR Failed.");
+		}
+		else
+		{
+			Debug.Log("XR initialized.");
+
+			Debug.Log("Starting XR...");
+			XRGeneralSettings.Instance.Manager.StartSubsystems();
+			Debug.Log("XR started.");
+		}
+	}
+
+	private void StopXR()
+	{
+		Debug.Log("Exiting VR mode");
+
+		Debug.Log("Stopping XR...");
+		XRGeneralSettings.Instance.Manager.StopSubsystems();
+		Debug.Log("XR stopped.");
+
+		Debug.Log("Deinitializing XR...");
+		XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+		Debug.Log("XR deinitialized.");
+
+		_Camera.ResetAspect();
+		_Camera.fieldOfView = _defaultFieldOfView;
+	}
 }
